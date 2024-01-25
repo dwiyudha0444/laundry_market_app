9462b1eb2cff761d2238b81f01f03111d2790559
dwiyudha0444 <yudhacamo12@gmail.com>
2024-01-25 08:36:08 +0700

    migration + model

---
+++ app/Models/Promo.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Promo extends Model
+{
+    protected $table = 'promos';
+    protected $fillable = [
+        'image',
+        'shop_id',
+        'old_price',
+        'new_price',
+        'description'
+    ];
+}
---
+++ app/Models/Shop.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Shop extends Model
+{
+    protected $table = 'shops';
+    protected $fillable = [
+        'image',
+        'name',
+        'location',
+        'city',
+        'delivery',
+        'pickup',
+        'whatsapp',
+        'description',
+        'price',
+        'rate',
+    ];
+}
