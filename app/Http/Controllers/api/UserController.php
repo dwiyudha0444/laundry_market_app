5c49c08ddcf5f53d002856b78d61c5239194f797
dwiyudha0444 <yudhacamo12@gmail.com>
2024-01-26 08:36:49 +0700

    Register & Login

--- app/Http/Controllers/api/UserController.php
+++ app/Http/Controllers/api/UserController.php
@@ -18,5 +18,42 @@ class UserController extends Controller
             'data' => $users,
         ], 200);
     }
+
+    function register(Request $request)
+    {
+        $this->validate($request, [
+            'username' => 'required|min:4|unique:users',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:8',
+        ]);
+
+        $user = User::create([
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return response()->json([
+            'data' => $user,
+        ], 201);
+    }
+
+    function login(Request $request)
+    {
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json([
+                'message' => 'Unauthorized'
+            ], 401);
+        }
+
+        $user = User::where('email', $request->email)->firstOrFail();
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'data' => $user,
+            'token' => $token,
+        ], 200);
+    }
     
 }
