88dea8d3f2447f160a221ce6ee828f6daf7b5c33
dwiyudha0444 <yudhacamo12@gmail.com>
2024-01-26 20:35:38 +0700

    middleware $ readLimit

--- routes/api.php
+++ routes/api.php
@@ -29,3 +29,16 @@ Route::get('/user', [UserController::class, 'readAll']);
 
 Route::post('/register', [UserController::class, 'register']);
 Route::post('/login', [UserController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    // Laundry
+    Route::get('/laundry/user/{id}', [LaundryController::class, 'whereUserId']);
+    Route::post('/laundry/claim', [LaundryController::class, 'claim']);
+
+    // Promo
+    Route::get('/promo/limit', [PromoController::class, 'readLimit']);
+
+    // Shop
+    Route::get('/shop/recommendation/limit', [ShopController::class, 'readRecommendationLimit']);
+    Route::get('/shop/search/city/{name}', [ShopController::class, 'searchByCity']);
+});
